0b4d2b75a9771aa781123f91f7d060d4e47a9a79
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-05-13 19:45:30 -0600

    Creación de lab_07 y primera parte

---
+++ main.c
@@ -0,0 +1,37 @@
+//*********************************************************************************************
+// Laboratorio 7
+// Helder Ovalle
+// 18349
+//*********************************************************************************************
+
+//*********************************************************************************************
+//Librerias
+//*********************************************************************************************
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/sysctl.c"
+#include "driverlib/interrupt.h"
+#include "driverlib/interrupt.c"
+#include "driverlib/gpio.h"
+#include "driverlib/gpio.c"
+#include "driverlib/timer.h"
+#include "driverlib/timer.c"
+#include "driverlib/uart.h"
+#include "driverlib/uart.c"
+#include "driverlib/pin_map.h"
+#include "driverlib/debug.h"
+#include "driverlib/rom.h"
+#include "grlib/grlib.h"
+#include <string.h>
+
+int main(void)
+{
+    return 0;
+}
+
+
