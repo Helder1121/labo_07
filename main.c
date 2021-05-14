70789ccebe25b2a28faaf6c779db190dc90817e5
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-05-14 14:58:49 -0600

    Lab terminado falta video y reporte

--- main.c
+++ main.c
@@ -28,36 +28,42 @@
 #include "driverlib/rom.h"
 #include "grlib/grlib.h"
 #include <string.h>
-
-//*********************************************************************************************
-//Variables
-//*********************************************************************************************
-int estaEncendido = false;
-char color='a';
-char antecolor='a';
 //*********************************************************************************************
 //Prototipos de funciones
 //*********************************************************************************************
+void UARTIntHandler(void);
 void Timer0IntHandler(void);
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count);
+//*********************************************************************************************
+//Variables
+//*********************************************************************************************
+int ON = false; //Estado del led
+char color='a'; //Color que se mostrara
 //*********************************************************************************************
 //Principal
 //*********************************************************************************************
 int main(void)
 {
-    //configuraciones
+    //Configuraciones
+    //Config clock
     SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ ) ;
+    //Puerto F habilitado
     SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOF ) ;
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
-    //UART
+    //Se habiita el UART
     SysCtlPeripheralEnable (SYSCTL_PERIPH_UART0);
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)){}
+    //Puerto A habilitado para el URTA
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    //Se habiita el RX y TX para la comunic.
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))
     GPIOPinConfigure(GPIO_PA0_U0RX);
     GPIOPinConfigure(GPIO_PA1_U0TX);
     IntMasterEnable();
     GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     UARTDisable(UART0_BASE);
+
+    //Config de los baudios a utilizar y demas parametros.
     UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
     IntEnable (INT_UART0);
     UARTIntEnable (UART0_BASE, UART_INT_RX);
@@ -66,6 +72,8 @@ int main(void)
     IntRegister(INT_UART0, UARTIntHandler);
     UARTFIFOEnable(UART0_BASE);
     UARTFIFOLevelSet(UART0_BASE,UART_FIFO_TX1_8,UART_FIFO_RX1_8);
+
+    //Config del TIMER0
     SysCtlPeripheralEnable (SYSCTL_PERIPH_TIMER0);
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
     SysCtlPeripheralReset (SYSCTL_PERIPH_TIMER0);
@@ -74,42 +82,56 @@ int main(void)
     TimerConfigure (TIMER0_BASE,TIMER_CFG_PERIODIC);
     TimerLoadSet (TIMER0_BASE, TIMER_A, 20000000 -1);
     TimerEnable (TIMER0_BASE, TIMER_A|TIMER_B);
-    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
-    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);//Establecer la ISR
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);//TIMER A
+    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);//Establecer la interrupcion
     IntEnable(INT_TIMER0A); //Habilitar interrupción por parte del NVIC
     TimerEnable(TIMER0_BASE, TIMER_A);//Habilitar temporizador
+
     //Configuración puertos salidas
-    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE,GPIO_PIN_1) ;//led rojo
-    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_2) ;//led azul
-    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_3) ;//led verde
+    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3) ;//rojo,azul y verde
     while(1){}
 }
 
+//Interrupcion del TIMER
 void Timer0IntHandler(){
     TimerIntClear(TIMER0_BASE,  TIMER_TIMA_TIMEOUT);
-        if (estaEncendido){
+        if (ON){
             GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x0);
         }else{
             switch(color){
                             case 'r':
                                 GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x02);
-                                antecolor='r';
                                 break;
                             case 'g':
                                 GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-                                antecolor='g';
                                 break;
                             case 'b':
                                 GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x04);
-                                antecolor='b';
                                 break;
                             case 'o':
                                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x0);
-                               antecolor='o';
                                 break;
                                 }
-
         }
-        estaEncendido = !estaEncendido;
-
-
+        ON = !ON;
+}
+//Interrupcion del UART
+void UARTIntHandler(){
+    uint32_t ui32Status;
+        //Inicia el estatus de la interrupcion
+        ui32Status = UARTIntStatus(UART0_BASE, true);
+        UARTIntClear(UART0_BASE, ui32Status);
+        while(UARTCharsAvail(UART0_BASE))
+        {
+            color=UARTCharGet(UART0_BASE);
+            UARTCharPutNonBlocking(UART0_BASE,color);
+        }
+}
+//Envio de datos para la tiva
+void UARTSend(const uint8_t *pui8Buffer, uint32_t ui32Count)
+{
+    while(ui32Count--)
+    {
+        UARTCharPutNonBlocking(UART0_BASE, *pui8Buffer++);
+    }
+}
