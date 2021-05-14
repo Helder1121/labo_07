6e3afa7381647fba273b54fcbdca0754a23badd7
Helder1121 <78274380+Helder1121@users.noreply.github.com>
2021-05-13 20:29:08 -0600

    parte 2

--- main.c
+++ main.c
@@ -29,9 +29,58 @@
 #include "grlib/grlib.h"
 #include <string.h>
 
+//*********************************************************************************************
+//Variables
+//*********************************************************************************************
+
+//*********************************************************************************************
+//Prototipos de funciones
+//*********************************************************************************************
+
+//*********************************************************************************************
+//Principal
+//*********************************************************************************************
 int main(void)
 {
-    return 0;
+    //configuraciones
+    SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ ) ;
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOF ) ;
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
+    //UART
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_UART0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)){}
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    IntMasterEnable();
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTDisable(UART0_BASE);
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    IntEnable (INT_UART0);
+    UARTIntEnable (UART0_BASE, UART_INT_RX);
+    UARTEnable (UART0_BASE);
+    IntPrioritySet(INT_UART0, 0x0);
+    IntRegister(INT_UART0, UARTIntHandler);
+    UARTFIFOEnable(UART0_BASE);
+    UARTFIFOLevelSet(UART0_BASE,UART_FIFO_TX1_8,UART_FIFO_RX1_8);
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_TIMER0);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)){}
+    SysCtlPeripheralReset (SYSCTL_PERIPH_TIMER0);
+    SysCtlDelay (5);
+    TimerDisable(TIMER0_BASE, TIMER_A|TIMER_B);
+    TimerConfigure (TIMER0_BASE,TIMER_CFG_PERIODIC);
+    TimerLoadSet (TIMER0_BASE, TIMER_A, 20000000 -1);
+    TimerEnable (TIMER0_BASE, TIMER_A|TIMER_B);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    TimerIntRegister(TIMER0_BASE, TIMER_A, Timer0IntHandler);//Establecer la ISR
+    IntEnable(INT_TIMER0A); //Habilitar interrupción por parte del NVIC
+    TimerEnable(TIMER0_BASE, TIMER_A);//Habilitar temporizador
+    //Configuración puertos salidas
+    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE,GPIO_PIN_1) ;//led rojo
+    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_2) ;//led azul
+    GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_3) ;//led verde
+    while(1){}
 }
 
 
